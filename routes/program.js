9c3f54ff21e7a65e512dead64c02b1a21218840b
hyelimirane <hyelimirane@gmail.com>
2015-12-07 22:01:33 +0900

    수정

--- routes/program.js
+++ routes/program.js
@@ -1,7 +1,20 @@
 var express = require('express');
+var sql = require('mssql');
 var router = express.Router();
 
-var data = [
+// db info
+var config = {
+    user: 'sa',
+    password: '1q@',
+    server: '192.168.35.201',
+    database: 'pam',
+    stream: true,
+    option: {
+        encrypt: true
+    }
+}
+
+/*var data = [
         {id:1, programName:'pro1', programExp:'pro1입니다', formName:'form1', formExp:'form1입니다', funName:'fun1', funExp:'fun1입니다'},
         {id:2, programName:'pro1', programExp:'pro1입니다', formName:'form1', formExp:'form1입니다', funName:'fun2', funExp:'fun2입니다'},
         {id:3, programName:'pro1', programExp:'pro1입니다', formName:'form2', formExp:'form2입니다', funName:'fun3', funExp:'fun3입니다'},
@@ -11,10 +24,37 @@ var data = [
         {id:7, programName:'pro3', programExp:'pro3입니다', formName:'form5', formExp:'form5입니다', funName:'fun7', funExp:'fun7입니다'},
         {id:8, programName:'pro4', programExp:'pro4입니다', formName:'form6', formExp:'form6입니다', funName:'fun8', funExp:'fun8입니다'}
     ];
+*/
+var data = [];
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('program', { title: 'Express' , data: data });
+    sql.connect(config, function(err){
+        var request = new sql.Request();
+        request.stream = true;
+        request.query('select * from prg_mng');
+
+        request.on('row', function(row){
+            console.log(row.pm_id);
+            data.push({'pm_id':row.pm_id, 'pm_prg_name':row.pm_prg_name, 'pm_prg_exp':row.pm_prg_exp, 'pm_form_name':row.pm_form_name, 'pm_form_exp':row.pm_form_exp, 'pm_fun_name':row.pm_fun_name, 'pm_fun_exp':row.pm_fun_exp});
+        });
+
+        /*request.on('recordset', function(columns){
+            console.log('----------------recordset-----------------');
+            console.dir(columns[1][1].prg_id);
+        });
+        */
+
+        request.on('error', function(err){
+            console.log('----------------error-----------------');
+            console.log(err) ;
+        });
+
+        request.on('done', function(returnValue){
+            console.log(data);
+            res.render('program', { title: 'Express' , data: data });
+        });
+    });
 });
 
 router.post('/edit', function(req, res, next){
