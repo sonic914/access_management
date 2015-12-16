c9f1259a279456d4c706c79f65ec2c4c093d6b87
hyelimirane <hyelimirane@gmail.com>
2015-12-16 20:43:05 +0900

    예외관리

---
+++ public/javascripts/js_exception.js
@@ -0,0 +1,172 @@
+/**
+ * Created by hyelim on 2015. 12. 15..
+ */
+/**
+ * 예외 관리 화면 script
+ */
+$(document).ready(function () {
+    $('#start_date').datepicker({
+        format: 'yyyy-mm-dd',
+        language: "kr"
+    });
+
+    $('#end_date').datepicker({
+        format: 'yyyy-mm-dd',
+        language: "kr"
+    });
+
+    $('div form div[show=\'after\'] ').each(function(index, item){
+       $(this).hide();
+    });
+    $('#saveExcUser').hide();
+
+    // 검색버튼 클릭 시
+    $('#searchExcUser').click(function(){
+        var sabun = $('#sabun').val();
+        if(sabun == ''){
+            alert('사번을 입력하세요.');
+            return false;
+        }
+        $.ajax({
+            url: '/exception/getUserGroup',
+            type: 'post',
+            data: {sabun:sabun},
+            success: function (data) {
+                var groups = data.data;
+                //debugger;
+                if(groups.length != 0){
+                    var option = '';
+                    for(var i=0; i<groups.length; i++){
+                        option += '<option>' + groups[i].USER_GROUP +' </option>'
+                    }
+                    $('#user_group option').remove();
+                    $('#user_group').append(option);
+                    $('#user_group option:first').attr('selected');
+
+                    getForbiddenPrg();
+                    getExcUserPrg();
+                }
+                else{
+                    alert('사번에 해당하는 그룹이 없습니다.');
+                    $('div form div[show=\'after\'] ').each(function(index, item){
+                        $(this).hide();
+                    });
+
+                    $('#saveExcUser').hide();
+                }
+            }
+        });
+    });
+
+    // 저장버튼 클릭 시
+    $('#saveExcUser').click(function(){
+        var data = {};
+        data.sabun = $('#sabun').val();
+        data.pgmSeq = $('#forbiddenPrgSelect').val();
+        data.startDate = $('#start_date').val();
+        data.endDate = $('#end_date').val();
+
+        var fullName = $('#exePrgListTbody tr td input[name=fullName]');
+        for(var i=0; i < fullName.length; i++){
+            var name = $('#forbiddenPrgSelect option:selected').attr('fullName');
+            if(name == fullName[i].value){
+                alert('해당 프로그램은 이미 예외처리 되었습니다.');
+                return false;
+            }
+        }
+        $.ajax({
+            url: '/exception/saveExcUser',
+            type: 'post',
+            data: data,
+            success: function (data) {
+                getForbiddenPrg();
+                getExcUserPrg();
+            }
+        });
+    });
+});
+
+function getForbiddenPrg(){
+    var userGroup = $('#user_group').val();
+
+    $.ajax({
+        url: '/exception/getForbiddenPrg',
+        type: 'post',
+        data: {userGroup:userGroup},
+        success: function (data) {
+            var list = data.data;
+
+            if(list.length > 0){
+                var option = '';
+                for(var i=0; i < list.length; i++){
+                    option += '<option value=\'' + list[i].pgm_seq + '\' fullName=\''+ list[i].prg_name +'\'>' + list[i].prg_exp +' </option>'
+                }
+                $('#forbiddenPrgSelect option').remove();
+                $('#forbiddenPrgSelect').append(option);
+
+                $('div form div[show=\'after\'] ').each(function(index, item){
+                    $(this).show();
+                });
+
+                $('#saveExcUser').show();
+            }
+            else{
+                alert('해당그룹에 제한 프로그램이 없습니다.');
+                $('div form div[show=\'after\'] ').each(function(index, item){
+                    $(this).hide();
+                });
+
+                $('#saveExcUser').hide();
+            }
+        }
+    });
+}
+
+function getExcUserPrg(){
+    var sabun = $('#sabun').val();
+
+    $.ajax({
+        url: '/exception/getExcUserPrg',
+        type: 'post',
+        data: {sabun:sabun},
+        success: function (data) {
+            var list = data.data;
+            var innerHTML = '';
+
+            if(list.length > 0){
+                for(var i=0; i < list.length; i++){
+                    innerHTML += '<tr>';
+                    innerHTML += '  <td>' + list[i].pm_prg_exp + '</td>';
+                    innerHTML += '  <td>' + list[i].pm_form_exp + '</td>';
+                    innerHTML += '  <td>' + list[i].pm_fun_exp + '</td>';
+                    innerHTML += '  <td>' + list[i].pum_str_date.substr(0, 10) + '</td>';
+                    innerHTML += '  <td>' + list[i].pum_end_date.substr(0, 10) + '</td>';
+                    innerHTML += '  <td> <input type=\'button\' onclick=\'delExcUserPrg(' + list[i].pum_seq +');\' class=\'btn btn-xs btn-primary\' value=\'삭제\' />';
+                    //innerHTML += '<input type=\'hidden\' name=\'pumSeq\' value=' + list[i].pum_seq + '>';
+                    innerHTML += '<input type=\'hidden\' name=\'fullName\' value=' + list[i].pm_prg_name + '_' + list[i].pm_form_name + '_' + list[i].pm_fun_name + '></td>';
+                    innerHTML += '</tr>';
+                }
+            }
+            else{
+                innerHTML += '<tr>';
+                innerHTML += '  <td colspan=\'6\'>조회 된 예외 프로그램이 없습니다.</td>';
+                innerHTML += '</tr>';
+            }
+            $('#exePrgListTbody').html(innerHTML);
+        }
+    });
+}
+
+
+function delExcUserPrg(pumSeq){
+    console.log(pumSeq);
+
+    $.ajax({
+        url: '/exception/delExcUserPrg',
+        type: 'post',
+        data: {pumSeq:pumSeq},
+        success: function (data) {
+            getExcUserPrg();
+        }
+    });
+}
--- routes/exception.js
+++ routes/exception.js
@@ -1,5 +1,27 @@
 var express = require('express');
+var sql = require('mssql');
+var mysql = require('mysql');
 var router = express.Router();
+
+// mssql db info
+var config = {
+    user: 'sa',
+    password: '1q@',
+    //server: '192.168.35.201',
+    server: 'hawaii402.ddns.net',
+    database: 'pam',
+    stream: true,
+    option: {
+        encrypt: true
+    }
+};
+
+var mysql_connection = mysql.createConnection({
+    //host: '192.168.35.201',
+    host: 'hawaii402.ddns.net',
+    user: 'hyelimirane',
+    password: 'yayayaya1@'
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -15,6 +37,164 @@ router.get('/', function (req, res, next) {
     }
 
     res.render('exception', {  link: linkLogin, btn: btnLogin  });
+});
+
+router.post('/getUserGroup', function (req, res) {
+    var sabun = req.body.sabun;
+
+    mysql_connection.query('use latte;');
+    var myQueryStr = 'select USER_GROUP ';
+    myQueryStr += 'from user_info ';
+    myQueryStr += 'where sabun_id = \'' + sabun + '\'';
+
+    mysql_connection.query(myQueryStr, function(err, rows){
+
+        res.send({data:rows});
+
+        if(err){
+            console.log('--------mysql error--------');
+            console.log(err);
+        }
+    });
+
+});
+
+router.post('/getForbiddenPrg', function (req, res) {
+    sql.connect(config, function (err) {
+
+        var userGroup = req.body.userGroup;
+
+        var request = new sql.Request();
+        request.stream = true;
+
+        var queryStr = '';
+        queryStr = 'select pgm.pgm_seq, pm.pm_id, concat(pm.pm_prg_name,\'_\', pm_form_name, \'_\',pm.pm_fun_name) prg_name,';
+        queryStr += ' concat(pm.pm_prg_exp,\'_\', pm_form_exp, \'_\',pm.pm_fun_exp) prg_exp';
+        queryStr += '  from prg_grp_mng pgm, prg_mng pm';
+        queryStr += ' where pgm.pgm_pm_id = pm.pm_id';
+        queryStr += '   and pgm.pgm_user_group = \'' + userGroup + '\'';
+        queryStr += '   and pgm.pgm_del_yn =\'N\'';
+
+        console.log('queryStr :: '+queryStr);
+        request.query(queryStr);
+
+        var data = [];
+
+        request.on('row', function (row) {
+            data.push({pgm_seq:row.pgm_seq, pm_id:row.pm_id, prg_name:row.prg_name, prg_exp:row.prg_exp});
+        });
+
+        request.on('error', function (err) {
+            console.log('----------------error-----------------');
+            console.log(err);
+        });
+
+        request.on('done', function (returnValue) {
+            res.send({title: 'Express', data:data});
+        });
+        //sql.close();
+    });
+});
+
+router.post('/saveExcUser', function (req, res) {
+    sql.connect(config, function (err) {
+
+        var inputData = req.body;
+
+        var request = new sql.Request();
+        request.stream = true;
+
+        var queryStr = '';
+        queryStr = 'insert into prg_user_mng (pum_pgm_seq, pum_sabun, pum_str_date, pum_end_date, pum_upt_id) values (';
+        queryStr += inputData.pgmSeq + ', \'' + inputData.sabun +'\', \'' + inputData.startDate + '\', \'' + inputData.endDate + '\', \'admin\')';
+        console.log(queryStr);
+        request.query(queryStr);
+
+        var data = [];
+        request.on('row', function (row) {
+            console.log(row);
+        });
+
+        request.on('error', function (err) {
+            console.log('----------------error-----------------');
+            console.log(err);
+        });
+
+        request.on('done', function (returnValue) {
+            res.send();
+        });
+        //sql.close();
+    });
+});
+
+router.post('/getExcUserPrg', function (req, res) {
+    sql.connect(config, function (err) {
+
+        var sabun = req.body.sabun;
+
+        var request = new sql.Request();
+        request.stream = true;
+
+        var queryStr = '';
+         queryStr += 'select pum.pum_seq, pm.pm_prg_name, pm.pm_prg_exp, pm.pm_form_name, pm.pm_form_exp,';
+        queryStr += ' pm.pm_fun_name, pm.pm_fun_exp, pum.pum_str_date, pum.pum_end_date';
+          queryStr += '  from prg_user_mng pum, prg_grp_mng pgm, prg_mng pm'; 
+        queryStr += ' where pum.pum_sabun = \'' + sabun + '\''; 
+        queryStr += '   and pum.pum_pgm_seq = pgm.pgm_seq'; 
+        queryStr += '   and pgm.pgm_pm_id = pm.pm_id';
+         queryStr += '   and pum.pum_del_yn = \'N\''; 
+        queryStr += '   and pgm.pgm_del_yn = \'N\''; 
+        queryStr += '   and pm.pm_del_yn = \'N\'';
+
+        console.log('queryStr :: '+queryStr);
+        request.query(queryStr);
+
+        var data = [];
+
+        request.on('row', function (row) {
+            data.push({pum_seq:row.pum_seq, pm_prg_name:row.pm_prg_name, pm_prg_exp:row.pm_prg_exp
+                , pm_form_name:row.pm_form_name, pm_form_exp:row.pm_form_exp, pm_fun_name:row.pm_fun_name, pm_fun_exp:row.pm_fun_exp
+                , pum_str_date:row.pum_str_date, pum_end_date:row.pum_end_date});
+        });
+
+        request.on('error', function (err) {
+            console.log('----------------error-----------------');
+            console.log(err);
+        });
+
+        request.on('done', function (returnValue) {
+            res.send({title: 'Express', data:data});
+        });
+        //sql.close();
+    });
+});
+
+router.post('/delExcUserPrg', function (req, res) {
+    sql.connect(config, function (err) {
+
+        var pumSeq = req.body.pumSeq;
+
+        var request = new sql.Request();
+        request.stream = true;
+
+        var queryStr = '';
+        queryStr += 'update prg_user_mng set pum_del_yn = \'Y\' where pum_seq =' + pumSeq;
+        console.log('queryStr :: '+queryStr);
+        request.query(queryStr);
+
+        request.on('row', function (row) {
+        });
+
+        request.on('error', function (err) {
+            console.log('----------------error-----------------');
+            console.log(err);
+        });
+
+        request.on('done', function (returnValue) {
+            res.send();
+        });
+        //sql.close();
+    });
 });
 
 module.exports = router;
